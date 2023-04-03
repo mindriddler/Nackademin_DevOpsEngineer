936d9e9870d1c3d08d82117c437e72eff6c3806e
Fredrik Magnusson <raekon92@gmail.com>
2023-04-03 22:29:54 +0200

    formated

--- 8_Linux1/Lektion_11/fredrik-labb1-1.sh
+++ 8_Linux1/Lektion_11/fredrik-labb1-1.sh
@@ -5,11 +5,11 @@ while [[ $# > 0 ]]; do
 	echo "Number of files in /etc starting with '$1' is: $num_of_files"
 	sum=0
 	for file in /etc/$1*; do
-	  if [ -f "$file" ]; then
-	    lines=$(wc -l < "$file")
-	    echo "File $file has $lines lines."
-	    ((sum += lines))
-	  fi
+		if [ -f "$file" ]; then
+			lines=$(wc -l <"$file")
+			echo "File $file has $lines lines."
+			((sum += lines))
+		fi
 	done
 	echo -e "Total number of lines: $sum\n"
 	shift
--- 8_Linux1/Lektion_11/fredrik-labb1-3.sh
+++ 8_Linux1/Lektion_11/fredrik-labb1-3.sh
@@ -3,7 +3,7 @@
 download_file() {
 	url=$1
 	filename=$(basename "$url")
-	curl -s -o "$filename" "$url" > /dev/null 2>&1 &
+	curl -s -o "$filename" "$url" >/dev/null 2>&1 &
 	pid=$!
 	echo -n "["
 	while [ -d /proc/$pid ]; do
@@ -20,9 +20,9 @@ if [[ $1 =~ "http://" || $1 =~ "https://" ]]; then
 	wait $download_pid
 	filename=$(basename "$url")
 	if [ -e "$filename" ]; then
-	  echo -e "\nDownload complete"
+		echo -e "\nDownload complete"
 	else
-	  echo -e "\nDownload failed"
+		echo -e "\nDownload failed"
 	fi
 else
 	echo "usage: $0 [URL]"
