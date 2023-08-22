d103463461e903d514f09b6a571cac6891f1dcf4
Fredrik Magnusson <fredrikmagnusson3@gmail.com>
2023-08-22 14:19:56 +0200

    changed the scripts abit

--- 10_Linux_2/lesson_1/multiplication.sh
+++ 10_Linux_2/lesson_1/multiplication.sh
@@ -2,9 +2,11 @@
 
 for i in {1..10}
 do
+	echo $i"ans tabell"
 	for j in {1..10}
 	do
 		value=$(($i*$j))
 		echo "$i * $j =" $value
 	done
+	echo ""
 done
--- 10_Linux_2/lesson_1/multiplication_hej.sh
+++ 10_Linux_2/lesson_1/multiplication_hej.sh
@@ -2,14 +2,16 @@
 
 for i in {1..10}
 do
+	echo $i"ans tabell"
 	for j in {1..10}
 	do
 		value=$(($i*$j))
 		if [ $i == $j ]
 		then
-			echo "hej"
-		#else
-			#echo "$i * $j =" $value
+			echo "hej ($i * $j)"
+		else
+			echo "$i * $j =" $value
 		fi
 	done
+	echo ""
 done
